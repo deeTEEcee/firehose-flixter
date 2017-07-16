6ea59d56370ec1b42df6f97b77064d937ca30f3c
David Chen <dchen@sightmachine.com>
2017-07-15 22:14:28 -0700

    add stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.reload_classes_only_on_change = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
